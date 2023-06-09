498591d6bd72809df3b3f6abdb718cf8f5c9cd70
Mesoma <okoyemeso33@gmail.com>
2023-06-09 11:56:00 +0100

    Hello_World

---
+++ lesson1/Client.kt
@@ -0,0 +1,19 @@
+import kotlinx.browser.document
+import react.create
+import react.dom.client.createRoot
+
+fun main() {
+    val container = document.createElement("div")
+    document.body!!.appendChild(container)
+
+    val welcome = Welcome.create {
+        name = "Kotlin/JS"
+    }
+    createRoot(container).render(welcome)
+}
+//The Lines Below Were Ran In Kotlon REPL
+fun printHello () {
+    println ("Hello World")
+}
+
+printHello()
